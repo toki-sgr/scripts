3b90f6223076da0851a9225bffe112b60b4fcc28
程昱博 <toki@nagisa.local>
2020-10-18 22:00:53 +0800

    [mart.] add HardGambler for further experiment

--- martingale.py
+++ martingale.py
@@ -1,4 +1,6 @@
 import random
+from statistics import *
+
 
 class BetLog:
     def __init__(self):
@@ -58,44 +60,101 @@ class BetLog:
                     line["balance"],
                     line["capital_after"]
                 )
-        print(output)
+        return output
 
-capital = 100000
-times = 0
-win_rate = 0.5
-power = 2
-origin_bet = 100
-bet = origin_bet
-last_won = True
+class HardGambler:
+    def __init__(self, win_rate = 0.5, power = 2, origin_capital = 100000, origin_bet = 100, time_limit = 100):
+        self.win_rate = win_rate
+        self.power = power
+        self.origin_capital = origin_capital
+        self.origin_bet = origin_bet
+        self.time_limit = time_limit
 
-betlog = BetLog()
-while times < 100 and capital > 0:
-    # get in new round
-    times += 1
+        self.betlogs = list()
+        self.explogs = list()
+        self.exp_result = dict()
 
-    # calculate bet this time
-    if last_won:
-        bet = origin_bet
-    else:
-        bet *= 2
-        # if not enough for doubling, all-in
-        if capital - bet <= 0:
-            bet = capital
-    betlog.push_preset(capital, bet)
-
-    # gambling section
-    capital -= bet
-    balance = 0
-    if random.random() < win_rate:
-        # won
-        balance = bet * power
-        capital += balance
+    def _gamble(self):
+        # initializing
+        betlog = BetLog()
+        times = 0
         last_won = True
-    else:
-        # lost
-        balance = - bet
-        last_won = False
-    betlog.push_result(last_won, balance)
-    betlog.submit()
-betlog.output()
+        bet = self.origin_bet
+        capital = self.origin_capital
 
+        while times < self.time_limit and capital > 0:
+            # get in new round
+            times += 1
+
+            # calculate bet this time
+            if last_won:
+                bet = self.origin_bet
+            else:
+                bet *= 2
+                # if not enough for doubling, all-in
+                if capital - bet <= 0:
+                    bet = capital
+            betlog.push_preset(capital, bet)
+
+            # gambling section
+            capital -= bet
+            balance = 0
+            if random.random() < self.win_rate:
+                # won
+                balance = bet * self.power
+                capital += balance
+                last_won = True
+            else:
+                # lost
+                balance = - bet
+                last_won = False
+            betlog.push_result(last_won, balance)
+            betlog.submit()
+        
+        # record result
+        self.betlogs.append(betlog.output())
+        self.explogs.append({
+            "times": times,
+            "final_capital": capital
+        })
+
+    def execute_experiment(self, exp_times = 1):
+        for i in range(exp_times):
+            self._gamble()
+
+        self.exp_result["exp_times"] = exp_times
+        self.exp_result["failed_times"] = 0
+        self.exp_result["total_balance"] = 0
+
+        times = list()
+        final_capitals = list()
+
+        for explog in self.explogs:
+            times.append(explog["times"])
+            final_capitals.append(explog["final_capital"])
+            if explog["final_capital"] == 0:
+                self.exp_result["failed_times"] += 1
+            self.exp_result["total_balance"] += explog["final_capital"] - self.origin_capital
+        self.exp_result["times_mean"] = mean(times)
+        self.exp_result["times_min"] = min(times)
+        self.exp_result["capital_mean"] = mean(final_capitals)
+        self.exp_result["capital_max"] = max(final_capitals)
+        self.exp_result["capital_stdev"] = int(stdev(final_capitals))
+        self.exp_result["capital_variance"] = int(variance(final_capitals))
+        
+
+
+    def output(self):
+        return self.betlogs, self.explogs, self.exp_result
+
+if __name__ == "__main__":
+    hg = HardGambler(win_rate = 0.5, power = 2, origin_capital = 100000, origin_bet = 100, time_limit = 100)
+    hg.execute_experiment(100)
+    betlogs, explogs, exp_result = hg.output()
+    
+    # if len(explogs) == 1:
+    #     print(betlogs[0])
+    # else:
+    #     for explog in explogs:
+    #         print(explog)
+    print(exp_result)
